2c268eced663d1fec50cb781335485fdd776c20b
PedroMakengo <pedromakengomiguel@gmail.com>
2025-04-29 22:51:11 +0100

    Adicionado o gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import { Header } from '@/components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
   return (
-    <div className='relative h-full  lg:h-[140vh]'>
-      <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
+    <div className='relative h-[140vh] h-full overflow-hidden bg-gradient-to-b'>
+      {/* <Header /> */}
+
+      <main className='relative pb-24 pl-4 lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
           <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
             <Image
               src='/banner.jpg'
-              alt='The Witcher'
+              alt='MAID'
               fill={true}
-              className='lg:h[95vh] left-0 top-0 z-0 h-[64vh] object-cover object-top'
+              className='h-[64vh] object-cover object-top lg:h-[95vh]'
             />
           </div>
 
